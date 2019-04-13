ebc85f11879cc49505891ed40ce92744995cae15
Marina Revazyan <maris.re@yandex.ru>
2019-04-13 22:12:57 +0300

    Deleted unnecessary variable Result

--- anagrams.py
+++ anagrams.py
@@ -3,12 +3,10 @@ You will be given two inputs a word and an array with words.
 You should return an array of all the anagrams or an empty array 
 if there are none. """
 
-
 from collections import Counter
 
 def anagrams(word, words):
     letters = Counter(word)
-    result = [i for i in words if Counter(i) == letters]
-    return result
+    return [i for i in words if Counter(i) == letters]
 
-# print(anagrams('abba',['aabb', 'abcd', 'bbaa', 'dada']))
+print(anagrams('abba',['aabb', 'abcd', 'bbaa', 'dada']))
